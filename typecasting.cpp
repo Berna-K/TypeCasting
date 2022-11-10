37895473d8efd0d00e52f47de654ad1246a993fc
ben <kimaryobernard@gmail.com>
2022-11-10 19:56:09 +0300

    typecasting

--- typecasting.cpp
+++ typecasting.cpp
@@ -3,10 +3,21 @@ using namespace std;
 
 int main()
 {
-float mynum =10.123;
+
+/*float mynum =10.123;
 
 cout<<mynum<<endl;
 int mynewnum= (int)mynum;
 cout<<mynewnum;
+*/
+int base=10,height=7, hypotenuse=12;
+
+int area = (float)1/2*(base*height);
+int perimeter = base+height+hypotenuse;
+cout<<"The area is: "<<area<<"\n";
+cout<<"The perimeter is: "<<perimeter<<"\n";
 return 0;
 }
+
+
+
